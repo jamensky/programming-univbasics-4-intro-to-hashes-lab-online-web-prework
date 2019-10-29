fee6c7bc2714ea29fad660ca9db172b86287ee3f
Jamal Rjoul <jamensky1991@gmail.com>
2019-10-29 21:50:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key](key)
-    hash += key
+  if hash(key)
+    hash[key] += key
   else 
-    hash[key]
+    hash[key] += key
   end
 end
